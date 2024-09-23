0345a8b60e32ffbbac2205795b5106b1f63df636
foysal90 <foysal1416@hotmail.com>
2024-09-22 23:12:41 -0400

    validation implemented

--- donate.js
+++ donate.js
@@ -10,30 +10,43 @@ const quotaInputField = document.getElementById("quota-input-field");
 const quotaDonate = document.getElementById("quotaDonate");
 console.log(quotaBalance.innerText);
 function donateMoney(event) {
-  const initialFund = parseFloat(initialAmount.innerText) || 0;
+  let initialFund = parseFloat(initialAmount.innerText) || 0;
   if (event.id === "donateNow") {
-    const donateAmountValue = parseFloat(donateAmount.value) || 0;
-    const remainingFund = initialFund - donateAmountValue;
-    initialAmount.innerText = remainingFund.toFixed(2);
-    noakhaliFloodDonate.innerText = parseFloat(
-      noakhaliFloodDonate.innerText + donateAmountValue
-    ).toFixed(2);
+    let donateAmountValue = parseFloat(donateAmount.value) || 0;
+    if (donateAmountValue > 0 && donateAmountValue <= initialFund) {
+      initialFund -= donateAmountValue;
+      initialAmount.innerText = initialFund.toFixed(2);
+      noakhaliFloodDonate.innerText = parseFloat(
+        noakhaliFloodDonate.innerText + donateAmountValue
+      ).toFixed(2);
+    } else {
+      return alert("invalid");
+    }
   } else if (event.id === "donateNowFeni") {
     //donation for feni flood
-    const donateAmountFeniValue =
+
+    let donateAmountFeniValue =
       parseFloat(donateAmountFeni.value).toFixed(2) || 0;
-    const remainingFund = initialFund - donateAmountFeniValue;
-    initialAmount.innerText = remainingFund.toFixed(2);
-    feniDonateAmount.innerText = parseFloat(
-      feniDonateAmount.innerText + donateAmountFeniValue
-    ).toFixed(2);
+    if (donateAmountFeniValue > 0 && donateAmountFeniValue <= initialFund) {
+      initialFund -= donateAmountFeniValue;
+      initialAmount.innerText = initialFund.toFixed(2);
+      feniDonateAmount.innerText = parseFloat(
+        feniDonateAmount.innerText + donateAmountFeniValue
+      ).toFixed(2);
+    } else {
+      return alert("invalid");
+    }
   } else if (event.id === "quotaDonate") {
-    const quotaInputFieldValue =
+    let quotaInputFieldValue =
       parseFloat(quotaInputField.value).toFixed(2) || 0;
-    const remainingFund = initialFund - quotaInputFieldValue;
-    initialAmount.innerText = remainingFund.toFixed(2);
-    quotaBalance.innerText = parseFloat(
-      quotaBalance.innerText + quotaInputFieldValue
-    ).toFixed(2);
+    if (quotaInputFieldValue > 0 && quotaInputFieldValue <= initialFund) {
+      initialFund -= quotaInputFieldValue;
+      initialAmount.innerText = initialFund.toFixed(2);
+      quotaBalance.innerText = parseFloat(
+        quotaBalance.innerText + quotaInputFieldValue
+      ).toFixed(2);
+    } else {
+      return alert("invalid");
+    }
   }
 }
