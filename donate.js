488fbfe5752ce282af1b2c78a3ae33098d67da83
foysal90 <foysal1416@hotmail.com>
2024-09-23 00:33:47 -0400

    modal implementedd

--- donate.js
+++ donate.js
@@ -8,7 +8,9 @@ const feniDonateAmount = document.getElementById("feniDonateAmount");
 const quotaBalance = document.getElementById("quota-balance");
 const quotaInputField = document.getElementById("quota-input-field");
 const quotaDonate = document.getElementById("quotaDonate");
-console.log(quotaBalance.innerText);
+
+let initialDonationModal = null;
+
 function donateMoney(event) {
   let initialFund = parseFloat(initialAmount.innerText) || 0;
   if (event.id === "donateNow") {
@@ -16,8 +18,8 @@ function donateMoney(event) {
     if (donateAmountValue > 0 && donateAmountValue <= initialFund) {
       initialFund -= donateAmountValue;
       initialAmount.innerText = initialFund.toFixed(2);
-      noakhaliFloodDonate.innerText = parseFloat(
-        noakhaliFloodDonate.innerText + donateAmountValue
+      noakhaliFloodDonate.innerText = (
+        parseFloat(noakhaliFloodDonate.innerText) + donateAmountValue
       ).toFixed(2);
     } else {
       return alert("invalid");
@@ -25,28 +27,44 @@ function donateMoney(event) {
   } else if (event.id === "donateNowFeni") {
     //donation for feni flood
 
-    let donateAmountFeniValue =
-      parseFloat(donateAmountFeni.value).toFixed(2) || 0;
+    let donateAmountFeniValue = parseFloat(donateAmountFeni.value) || 0;
     if (donateAmountFeniValue > 0 && donateAmountFeniValue <= initialFund) {
       initialFund -= donateAmountFeniValue;
       initialAmount.innerText = initialFund.toFixed(2);
-      feniDonateAmount.innerText = parseFloat(
-        feniDonateAmount.innerText + donateAmountFeniValue
+      feniDonateAmount.innerText = (
+        parseFloat(feniDonateAmount.innerText) + donateAmountFeniValue
       ).toFixed(2);
+      // alert("donated");
     } else {
       return alert("invalid");
     }
   } else if (event.id === "quotaDonate") {
-    let quotaInputFieldValue =
-      parseFloat(quotaInputField.value).toFixed(2) || 0;
+    let quotaInputFieldValue = parseFloat(quotaInputField.value) || 0;
     if (quotaInputFieldValue > 0 && quotaInputFieldValue <= initialFund) {
       initialFund -= quotaInputFieldValue;
       initialAmount.innerText = initialFund.toFixed(2);
-      quotaBalance.innerText = parseFloat(
-        quotaBalance.innerText + quotaInputFieldValue
+      quotaBalance.innerText = (
+        parseFloat(quotaBalance.innerText) + quotaInputFieldValue
       ).toFixed(2);
     } else {
       return alert("invalid");
     }
+  }
+}
+
+function openModal(eventId) {
+  initialDonationModal = eventId;
+  const modal = document.getElementById("my_modal_1");
+  modal.showModal();
+}
+function closeModal() {
+  const modal = document.getElementById("my_modal_1");
+  modal.close();
+}
+function confirmDonation() {
+  if (initialDonationModal) {
+    const event = { id: initialDonationModal };
+    donateMoney(event);
+    closeModal();
   }
 }
